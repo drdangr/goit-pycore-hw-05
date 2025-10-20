2fcc581cd122e54212a144c28bbe0ff51e4f6633
drdangr <drdangr@gmail.com>
2025-10-20 13:30:48 +0300

    task1 comm debug prints

--- Task1_Fib.py
+++ Task1_Fib.py
@@ -13,7 +13,7 @@ def caching_fibonacci():
         if n in cache:
             
             # debug print
-            print(f"   - Retrieving from cache: fib({n}) = {cache[n]}")
+            # print(f"   - Retrieving from cache: fib({n}) = {cache[n]}")
             
             return cache[n]
         
@@ -21,7 +21,7 @@ def caching_fibonacci():
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
         
         #debug print
-        print(f"Calculating and caching: fib({n}) = {cache[n]}")
+        #print(f"Calculating and caching: fib({n}) = {cache[n]}")
         
         return cache[n]
     
@@ -34,6 +34,6 @@ print(fib(10))  # 55
 print(fib(15))  # 610
 
 # Проверка кэша
-print(fib(10))  # Должно извлечь из кэша
+print(fib(10))  # Должно извлечь из кэша, если раскоментировать debug print, будет видно
 print(fib(14))  # Должно извлечь из кэша
 print(fib(11))  # Должно извлечь из кэша
